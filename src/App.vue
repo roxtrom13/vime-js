9d82ad72bdbeff91297e6a5130b3113692795d6a
Roy Palomino <roy100995@gmail.com>
2021-10-07 19:45:32 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Player, Youtube, Vimeo } from '@vime/vue-next';
+
+export default defineComponent({
+  setup() {
+
+  },
+  components: {
+    Player,
+    Youtube,
+    Vimeo
+  }
+})
+</script>
+
+
+<template lang="pug">
+Player(controls)
+  Vimeo(videoId="411652396")
+
+Player(controls)
+  Youtube(videoId="DyTCOwB0DVw")
+
+</template>
+
+<style>
+</style>
